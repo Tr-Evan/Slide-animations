905f1a2f3770b9e998a65fcc2d4a4343a736c679
Evan Troget <122986796+Tr-Evan@users.noreply.github.com>
2023-10-06 14:35:46 +0000

    Update 9 files

--- js/demo4/index.js
+++ js/demo4/index.js
@@ -1,4 +1,4 @@
-import { preloadImages } from "../utils.js"; // Utility function to preload images
+import { preloadImages } from "../utils.js"; //preload images
 import { Slideshow } from "./slideshow.js";
 
 const slides = document.querySelector(".slides");
@@ -10,17 +10,15 @@ document
 document
   .querySelector(".slides-nav__item--next")
   .addEventListener("click", () => slideshow.next());
-// Initialize the GSAP Observer plugin
+
 Observer.create({
   type: "wheel,touch,pointer",
   onDown: () => slideshow.prev(),
   onUp: () => slideshow.next(),
-  // invert the mouse wheel delta
   wheelSpeed: -1,
-  tolerance: 10
+  tolerance: 10,
 });
 
-// Preload all images. Once all images are preloaded, remove the 'loading' class from the body.
 preloadImages(".slide__img").then(() =>
-  document.body.classList.remove("loading")
+  document.body.classList.remove("loading"),
 );
